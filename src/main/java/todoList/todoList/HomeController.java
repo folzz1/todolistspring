7e4058291960132d2896bc72f30facee28efac57
folzz1 <folzz0463@gmail.com>
2024-09-09 17:39:50 +0300

    test update

--- src/main/java/todoList/todoList/HomeController.java
+++ src/main/java/todoList/todoList/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/second")
+    public String secondPage() {
+        return "second";
+    }
 }
